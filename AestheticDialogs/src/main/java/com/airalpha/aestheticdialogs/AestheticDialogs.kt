12b267c669204b7622b96bfca11816b087fcd77f
AIRALPHA <allphaair@gmail.com>
2023-12-20 20:31:16 +0100

    rainbow success

--- AestheticDialogs/src/main/java/com/airalpha/aestheticdialogs/AestheticDialogs.kt
+++ AestheticDialogs/src/main/java/com/airalpha/aestheticdialogs/AestheticDialogs.kt
@@ -1,9 +1,113 @@
 package com.airalpha.aestheticdialogs
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
 class AestheticDialogs {
 
-    fun test(): String {
-        return "hey"
+    fun Builder() {
+
     }
+
+    @Composable
+    fun RainbowDialog() {
+        var visible by remember { mutableStateOf(true) }
+
+        Dialog(
+            onDismissRequest = { visible = false },
+            properties = DialogProperties(usePlatformDefaultWidth = false)
+        ) {
+            AnimatedVisibility(visible = visible, enter = slideInVertically(), exit = fadeOut()) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.TopCenter
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp)
+                            .fillMaxWidth()
+                            .height(100.dp)
+                            .background(color = colorResource(id = R.color.dialog_success))
+                            .padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Row(
+                            modifier = Modifier.weight(.3f),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(16.dp)
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_check_circle_green_24dp),
+                                contentDescription = "Success",
+                                modifier = Modifier.size(30.dp),
+                                tint = colorResource(id = R.color.md_white_1000)
+                            )
+                            Column {
+                                Text(
+                                    text = "Success",
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = colorResource(id = R.color.md_white_1000)
+                                )
+                                Text(
+                                    text = "The message was sent successfully !",
+                                    fontSize = 14.sp,
+                                    maxLines = 2,
+                                    lineHeight = 18.sp,
+                                    color = colorResource(id = R.color.md_white_1000)
+                                )
+                            }
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_close_gray_24dp),
+                                contentDescription = "Close",
+                                modifier = Modifier.size(30.dp),
+                                tint = colorResource(id = R.color.md_white_1000)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 
 }
--- app/src/main/java/com/airalpha/sample/MainActivity.kt
+++ app/src/main/java/com/airalpha/sample/MainActivity.kt
@@ -3,13 +3,16 @@ package com.airalpha.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.airalpha.aestheticdialogs.AestheticDialogs
 import com.airalpha.sample.ui.theme.AestheticDialogsTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +21,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             AestheticDialogsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Success")
+                    }
+
+                    AestheticDialogs().RainbowDialog()
                 }
             }
         }
